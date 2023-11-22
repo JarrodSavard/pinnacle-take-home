58d1d742a357ba779541c831d795bc66a6e1845a
JarrodSavard <jarrodsavard2x180@gmail.com>
2023-11-21 16:03:01 -0800

    added trim to search query

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -12,7 +12,7 @@ export const SearchBar = () => {
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = event.target.value;
-        setQuery(inputValue);
+        setQuery(inputValue.trim());
 
         if (timer) {
             clearTimeout(timer);
